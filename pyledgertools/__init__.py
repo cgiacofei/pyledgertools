6c196a7bd9910f843b2b8514a9243a910d369047
chris <c.giacofei@gmail.com>
2017-03-10 21:06:45 -0500

    Add some basic python package stuff.

---
+++ pyledgertools/__init__.py
@@ -0,0 +1,4 @@
+"""pyledgertools init file"""
+
+from pyledgertools.ofx2ledger import build_journal
+from pyledgertools.ofx2ledger import export_journal
---
+++ setup.py
@@ -0,0 +1,35 @@
+"""pyledgertools setup file"""
+
+from setuptools import setup
+
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
+setup(name='pyledgertools',
+      version='0.1',
+      description='Python based tools for ledger accounting.',
+      long_description=readme(),
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: Public Domain',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Office/Business :: Financial :: Accounting',
+      ],
+      keywords='ledger-cli plaintextaccounting ofx',
+      url='http://github.com/cgiacofei/pyledgertools',
+      author='Chris Giacofei',
+      author_email='c.giacofei@gmail.com',
+      license='Public Domain',
+      packages=['pyledgertools'],
+      install_requires=[
+        'hashlib',
+        'ofx_tools',
+        'optparse',
+        'PyYaml',
+      ],
+      include_package_data=True,
+      zip_safe=False
+)
