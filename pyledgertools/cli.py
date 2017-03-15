7d40bd3a4e83beb5b0bbcafbe1361f657488d8ac
Chris Giacofei <c.giacofei@gmail.com>
2017-03-15 12:14:25 -0400

    Sphinx complaining about top level code.
    Moved top level code into main().

--- pyledgertools/cli.py
+++ pyledgertools/cli.py
@@ -3,38 +3,39 @@
 from argparse import ArgumentParser
 from yaml import load
 
-from pyledgertools.ofx2leddger import build_journal
+from pyledgertools.ofx2ledger import build_journal
 
-parser = ArgumentParser()
+def main():
 
-parser.add_option(
-    "-i", "--input-ofx",
-    dest="ofx_file",
-    default=None,
-    help=".OFX file to parse"
-)
-parser.add_option(
-    "-l", "--ledger-file",
-    dest="ledger_file",
-    default=None,
-    help="Ledger file to check transactions against."
-)
-parser.add_option(
-    "-c", "--config",
-    dest="config",
-    default=None,
-    help="config file for account names/numbers etc."
-)
-parser.add_option(
-    "-r", "--rules",
-    dest="rule_file",
-    default='rules.txt',
-    help="File or directory containing matching rules."
-)
+    parser = ArgumentParser()
 
-options = parser.parse_args()
+    parser.add_argument(
+        "-i", "--input-ofx",
+        dest="ofx_file",
+        default=None,
+        help=".OFX file to parse"
+    )
+    parser.add_argument(
+        "-l", "--ledger-file",
+        dest="ledger_file",
+        default=None,
+        help="Ledger file to check transactions against."
+    )
+    parser.add_argument(
+        "-c", "--config",
+        dest="config",
+        default=None,
+        help="config file for account names/numbers etc."
+    )
+    parser.add_argument(
+        "-r", "--rules",
+        dest="rule_file",
+        default='rules.txt',
+        help="File or directory containing matching rules."
+    )
 
-if __name__ == "__main__":
+    options = parser.parse_args()
+
     if options.config:
         c_path = options.config
 
@@ -45,3 +46,8 @@ if __name__ == "__main__":
     config = load(open(c_path, 'r'))
 
     build_journal(options.ofx_file, config['accounts'])
+
+
+if __name__ == "__main__":
+    main()
+
