694dc6adfbe79fa65a8fd37f4c7960eb45e9ed31
U-MNAO\cgiacofe <cgiacofe@D9-JJXMW52.mnao.net>
2017-03-21 11:04:30 -0400

    Modify cli.py to use the new plugins.

--- pyledgertools/cli.py
+++ pyledgertools/cli.py
@@ -1,14 +1,11 @@
 #! /usr/bin/env python3
 
 from argparse import ArgumentParser
+from configparser import ConfigParser
 import os
 from os.path import expanduser
-from yaml import load
+import sys
 from yapsy.PluginManager import PluginManager
-from yapsy.IPlugin import IPlugin
-
-from pyledgertools.ofx2ledger import build_journal
-from pyledgertools.plugin import IDownload, IClassify, IModify
 
 DIR_PATH = os.path.dirname(os.path.realpath(__file__))
 
@@ -39,7 +36,7 @@ def get_args():
     parser.add_argument(
         "-r", "--rules",
         dest="rule_file",
-        default='rules.txt',
+        default=None,
         help="File or directory containing matching rules."
     )
 
@@ -48,78 +45,115 @@ def get_args():
     return args
 
 
-def main():
-    """Run the command line interface."""
-    # Build the manager.
-    plugin_manager = PluginManager()
+def get_plugin(manager, name):
+    """Find a plugin by name."""
+    for plugin in manager.getAllPlugins():
+        if plugin.name == name:
+            return plugin.plugin_object
 
-    plugin_manager.setCategoriesFilter({
-        'classify': IClassify,
-        'download': IDownload,
-        'modify': IModify,
-    })
-
-    # Tell it the default place(s) where to find plugins.
-    plugin_manager.setPluginPlaces([
-        os.path.join(DIR_PATH, 'plugins')
-    ])
-
-    plugin_manager.collectPlugins()
-
-    print('All', [x.name for x in plugin_manager.getAllPlugins()])
-    print('Download', [x.name for x in plugin_manager.getPluginsOfCategory('download')])
-    print('Classify', [x.name for x in plugin_manager.getPluginsOfCategory('classify')])
-    print('Modify', [x.name for x in plugin_manager.getPluginsOfCategory('modify')])
-
-    from configparser import ConfigParser
-
-    config = ConfigParser()
-
-    home = expanduser("~")
-
-    args = get_args()
-
-    if args.config:
-        c_path = args.config
-
-    else:
-        ROOT = os.path.dirname(os.path.realpath(__file__))
-        c_path = os.path.join(home,'.config','ofxtools', 'ofxget.conf')
-
-    config.read(c_path)
-
-    return build_journal(args.ofx_file, config)
+    return None
 
 
 def interactive():
-    import sys
-    from pyledgertools.classifier import Classifier
-    from pyledgertools.rule_parser import walk_rules, build_rules
+    """Run the command line interface."""
+    # Load Plugins
+    manager = PluginManager()
+    manager.setPluginPlaces([
+        os.path.join(DIR_PATH, 'plugins')
+    ])
+
+    manager.collectPlugins()
+
+    # Load classification plugins
+    rule = get_plugin(manager, 'Rule Based Classifier')
+    bayes = get_plugin(manager, 'Naive Bayes Classifier')
 
     args = get_args()
-    bal, trans = main()
 
-    trans.sort(key=lambda x: x.date)
+    # Load Config file
+    if args.config:
+        c_path = args.config
+    else:
+        c_path = os.path.join(
+            expanduser("~"), '.config', 'ofxtools', 'ofxget.conf'
+        )
+
+    config = ConfigParser()
+    config.read(c_path)
+
+    balances = []
+    transactions = []
+
+
+
+    # -------------------------------------------------------------------------
+    # Test suntrust
+    # -------------------------------------------------------------------------
+    conf = dict(config.items('fixed'))
+    try:
+        global_conf = dict(config.items(conf['global']))
+        conf.update(global_conf)
+    except KeyError:
+        pass
+
+    try:
+        parent_conf = dict(config.items(conf['parent']))
+        conf.update(parent_conf)
+    except KeyError:
+        pass
+
+    
+
+    getter = get_plugin(manager, conf['downloader'])
+    parser = get_plugin(manager, conf['parser'])
+
+    file_path = getter.download()
+
+    bal, trans = parser.build_journal(file_path, conf)
+
+    balances = balances + bal
+    transactions = transactions + trans
+
+    # -------------------------------------------------------------------------
+    # Test cap one checking
+    # -------------------------------------------------------------------------
+    #~ conf = dict(config.items('flex'))
+
+    #~ getter = get_plugin(manager, conf['downloader'])
+    #~ parser = get_plugin(manager, conf['parser'])
+
+    #~ file_path = getter.download()
+
+    #~ bal, trans = parser.build_journal(file_path, conf)
+
+    #~ balances = balances + bal
+    #~ transactions = transactios + trans
+
+    transactions.sort(key=lambda x: x.date)
 
     if args.ledger_file is not None:
-        interactive_classifier = Classifier(journal_file=args.ledger_file)
+        interactive_classifier = bayes.setup(journal_file=args.ledger_file)
     else:
-        interactive_classifier = Classifier()
+        interactive_classifier = bayes.setup()
 
-    for posting in trans:
+    if args.rule_file is not None:
+        rules = rule.build_rules(args.rule_file)
+    else:
+        rules = None
+
+    for transaction in transactions:
         match = None
         result = None
-        text = posting.payee
-        amount = posting.allocations[0].amount
-        currency = posting.allocations[0].currency
+        text = transaction.payee
+        amount = transaction.postings[0].amount
+        currency = transaction.postings[0].currency
 
-        if args.rule_file is not None:
-            rules = build_rules(args.rule_file)
 
-        for rule in rules.keys():
-            match = walk_rules(rules[rule]['conditions'], posting)
+
+        for entry in rules.keys():
+            match = rule.walk_rules(rules[entry]['conditions'], transaction)
             if match:
-                found_rule = rules[rule]
+                found_rule = rules[entry]
                 break
                 sys.exit()
             else:
@@ -130,14 +164,14 @@ def interactive():
         except KeyError:
             skip = False
 
-        if (args.rule_file is None or found_rule == {}) and skip == False:
+        if (args.rule_file is None or found_rule == {}) and skip is False:
             result = interactive_classifier.classify(text, method='bayes')
 
         print('')
         print('=' * 80)
-        print(posting.to_string())
+        print(transaction.to_string())
         print('')
-        if skip == True:
+        if skip is True:
             print('Skip transfer Deposit')
             pass
         elif result is None:
@@ -167,20 +201,18 @@ def interactive():
             print('Using ', account)
             print('')
 
-
             interactive_classifier.update(text, account)
 
-            posting.add(account, amount * -1, currency)
+            transaction.add(account, amount * -1, currency)
 
             print('---------------------')
-            print(posting.to_string())
+            print(transaction.to_string())
             with open('tmp.ledger', 'a') as outfile:
-                print(posting.to_string(), '\n', file=outfile)
+                print(transaction.to_string(), '\n', file=outfile)
 
         print('')
         print('=' * 80)
 
 
 if __name__ == "__main__":
-    main()
-
+    interactive()
