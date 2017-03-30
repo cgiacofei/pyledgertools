c77190291d0f5e53a4b1e3209506f42ecff47a84
chris <c.giacofei@gmail.com>
2017-03-30 06:12:24 -0400

    Add larger skeleton to ADP parser.

--- pyledgertools/plugins/process/adp.py
+++ pyledgertools/plugins/process/adp.py
@@ -1,7 +1,25 @@
 """Process ADP paystub json."""
-
+import glob
+import os
 from yapsy.IPlugin import IPlugin
 
-class ProcessADP(IPlugin):
-    """Main plugin class"""
-    pass
+
+class ProcessADP(object):
+    """Main ADP paycheck parser class."""
+
+    def __init__(self, config):
+        self.stub_dir = config['global']['paystubs']
+
+    def get_transaction(self):
+        pass
+
+    def _lookup_stub(self, transaction):
+        """Match paystub with transaction."""
+
+        glob_dir = os.path.join([self.stub_dir, '**', '*.json'])
+        for json_file in glob.iglob(glob_dir, recursive=True):
+            pass
+            # Do processing
+
+    def _parse_stub(self, stub_json):
+        pas
