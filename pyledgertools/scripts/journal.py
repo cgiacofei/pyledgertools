7606d95006297270a841e14ccaba1370d6c6379d
Chris <c.giacofei@gmail.com>
2017-04-06 11:26:24 -0400

    Add script to output date sorted journal file.

---
+++ pyledgertools/scripts/journal.py
@@ -0,0 +1,25 @@
+from argparse import ArgumentParser
+
+def sort_journal():
+    """Sort journal file transactions."""
+
+    parser = ArgumentParser()
+
+    parser.add_argument(
+        '-j', '--journal-file',
+        dest='journal_file',
+        default=None,
+        help='Journal file to be sorted.'
+    )
+    args = dict((k, v) for k, v in vars(parser.parse_args()).items() if v)
+    journal_file = args.get('journal_file')
+
+    with open(journal_file, 'r') as infile:
+        journal_string = infile.read()
+
+    blocks = [x for x in journal_string.split('\n\n')]
+
+    blocks.sort()
+
+    for tran in blocks:
+        print(tran, '\n')
