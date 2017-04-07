65bb614156c9d0550575d3d78e717202d26fa60a
chris <c.giacofei@gmail.com>
2017-04-06 22:14:53 -0400

    Fix auto-matching.
    For real this time. The first list element was being cut off.

--- pyledgertools/cli.py
+++ pyledgertools/cli.py
@@ -201,7 +201,7 @@ def interactive():
 
                 if all(x in [False, None] for x in [skip, process, allocations]):
                     result = interactive_classifier.classify(text, method='bayes')
-                    cleaned = [x for x in result[1:] if round(x[1], 10) > 0]
+                    cleaned = [x for x in result if round(x[1], 10) > 0]
                     if len(cleaned) > 0:
                         result = cleaned
 
