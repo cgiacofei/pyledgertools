ec63791a3aee1bb5fb920be3560ab8eb2a036296
Chris <c.giacofei@gmail.com>
2017-07-06 08:45:56 -0400

    Fix lots of bugs in mortgage plugin.
     - Dates are string formatted, not datetime objects.
     - Needed to strip '$' from csv file values.
     - Wrong sign when calculating remaining balance.
     - Pull rule arguments correctly.
    
    Seems to work as expected now.

--- pyledgertools/plugins/process/mortgage.py
+++ pyledgertools/plugins/process/mortgage.py
@@ -14,48 +14,52 @@ strptime = datetime.strptime
 class AmortSchedule(IPlugin):
     """OFX file parsing."""
 
-    def process(self, transaction, config, **args):
+    def process(self, transaction, config, rule_args):
         """Get matching line from schedule.
 
-        args: arguments from rules fils
+        rule_args: arguments from rules fils
             file: mortgage schedule csv file.
+            currency: Currency to use for postings. (default to '$')
             <csv_column>: <ledger:account:name>
         """
         self.config = config
-
         logging.config.dictConfig(config.get('logging', None))
         self.logger = logging.getLogger(__name__)
 
         date = transaction.date
         self.logger.info(
-            'Transaction date: {}'.format(strftime(date, '%Y-%m-%d'))
+            'Transaction date: {}'.format(date)
         )
-        schedule_csv = args.pop('file', None)
-        currency = args.pop('currency', '$')
+        schedule_csv = rule_args.pop('file', None)
+        currency = rule_args.pop('currency', '$')
 
+        self.logger.info(
+            'Reading schedule from {}'.format(schedule_csv)
+        )
         with open(schedule_csv, 'r') as f:
             schedule = csv.DictReader(f)
             row = min(
                 list(schedule),
-                key=lambda x: abs(strptime(x['date'], '%Y-%m-%d') - date)
+                key=lambda x: abs(strptime(x['date'], '%Y-%m-%d') - strptime(date, '%Y-%m-%d'))
             )
-            self.logger.info('Selected amortization row. ' + str(row))
+            self.logger.debug('Selected amortization row. ' + str(row))
 
-        running_sum = 0
-        for column in args.keys():
+        running_sum = 0.00
+        for column in rule_args.keys():
             try:
-                transaction.add(args[column], row[column], currency)
-                running_sum += float(row[column])
+                row_value = float(row[column].replace('$', ''))
+                transaction.add(rule_args[column], row_value, currency)
+                running_sum += row_value
                 self.logger.info(
-                    'Add posting: {} {}'.format(args[column], row[column])
+                    'Add posting: {}  {} {}'.format(rule_args[column], currency, row_value)
                 )
             except KeyError:
                 # If column is not in csv file use the remaining total balance.
                 # This must happen last and only ONCE per transaction.
-                value = transaction.postings[0].amount - running_sum
-                transaction.add(args[column], value, currency)
+                value = abs(transaction.postings[0].amount) - abs(running_sum)
+                transaction.add(rule_args[column], value, currency)
                 self.logger.info(
-                    'Add posting: {} {}'.format(args[column], value)
+                    'Add posting: {} {}'.format(rule_args[column], value)
                 )
 
         return transaction
